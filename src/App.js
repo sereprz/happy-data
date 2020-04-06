796b0a3aca46fbcdd9b2f44ba521553b815ac713
sereprz <sereprz@gmail.com>
2020-04-06 14:13:07 -0400

    Use flak endpoint in React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg'; //not in use
 import './App.css';
 
 function App() {
-  return (
+  const [currentTime, setCurrentTime] = useState(0);
+  
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    })
+  },[]);
+
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Current Time
+        <p>The current time is: {currentTime}</p>
       </header>
     </div>
-  );
+    );
 }
 
 export default App;
